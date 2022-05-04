0c11e9cad22b3d0d79dac4bb5dd6f52befc183b0
Erik Lindmaa <guslinerz@student.gu.se>
2022-05-04 11:12:49 +0200

    #26 Remove Adapter. Add emergency contact class and databasehelper

---
+++ android/app/src/main/java/platis/solutions/smartcarmqttcontroller/Data/DataBaseHelper.java
@@ -0,0 +1,126 @@
+package platis.solutions.smartcarmqttcontroller.Data;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import platis.solutions.smartcarmqttcontroller.Model.EmergencyContact;
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+
+    public static final String CONTACT_TABLE = "CONTACT_TABLE";
+    public static final String COLUMN_CONTACT_FIRSTNAME = "CONTACT_NAME";
+    public static final String COLUMN_CONTACT_LASTNAME = "CONTACT_LASTNAME";
+    public static final String COLUMN_CONTACT_PHONE_NUMBER = "CONTACT_PHONE_NUMBER";
+    public static final String COLUMN_CONTACT_EMAIL = "CONTACT_EMAIL";
+    public static final String COLUMN_ID = "ID";
+
+    public DataBaseHelper(@Nullable Context context) {
+        super(context, "contact.db", null, 1);
+    }
+
+    //this is called the first time a database is accessed. There should be code in here to create a new database.
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTableStatement = "CREATE TABLE " + CONTACT_TABLE + " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_CONTACT_FIRSTNAME + " TEXT, " + COLUMN_CONTACT_LASTNAME + " TEXT, " + COLUMN_CONTACT_PHONE_NUMBER + " INT, " + COLUMN_CONTACT_EMAIL + " TEXT)";
+        onUpgrade(db,-1,0);
+        db.execSQL(createTableStatement);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + CONTACT_TABLE);
+    }
+
+
+    public boolean addOne(EmergencyContact contactModel){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_CONTACT_FIRSTNAME, contactModel.getFirst_name());
+        cv.put(COLUMN_CONTACT_LASTNAME, contactModel.getLast_name());
+        cv.put(COLUMN_CONTACT_PHONE_NUMBER, contactModel.getPhone_number());
+        cv.put(COLUMN_CONTACT_EMAIL, contactModel.getEmail());
+
+
+        long insert = db.insert(CONTACT_TABLE, null, cv);
+        if(insert == -1){
+            return false;
+        }else{
+            return true;
+        }
+
+    }
+
+
+    public List<EmergencyContact> getEveryone(){
+
+        List<EmergencyContact> returnList = new ArrayList<>();
+
+        //get data from the database
+
+        String queryString = "SELECT * FROM " + CONTACT_TABLE;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(queryString, null);
+
+        if(cursor.moveToFirst()){
+            //loop through the cursor (result set) and create new customer objects. Put them into the return list.
+            do{
+                int contactID = cursor.getInt(0);
+                String contact_firstName = cursor.getString(1);
+                String contact_lastName = cursor.getString(2);
+                int contact_phoneNumber = cursor.getInt(3);
+                String contact_email = cursor.getString(4);
+
+                //boolean customerActive = cursor.getInt(3) == 1 ? true: false;
+
+                EmergencyContact contact = new EmergencyContact(contactID, contact_firstName, contact_lastName, contact_phoneNumber, contact_email);
+                returnList.add(contact);
+
+            } while (cursor.moveToNext());
+
+
+        }else{
+            //failure. do not add anything to the list.
+        }
+
+        //close both the cursor and db when done
+
+        cursor.close();
+        db.close();
+
+        return returnList;
+
+    }
+
+    public boolean deleteOne(EmergencyContact contactModel){
+        //find customerModel in the database. If it is found, delete it and return true.
+        //if it is not found, return false
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        String queryString = "DELETE FROM " + CONTACT_TABLE + " WHERE " + COLUMN_ID + " = " + contactModel.getId();
+
+        Cursor cursor = db.rawQuery(queryString, null);
+
+        if(cursor.moveToNext()){
+            return true;
+
+        }else{
+            return false;
+        }
+
+    }
+
+
+}
---
+++ android/app/src/main/java/platis/solutions/smartcarmqttcontroller/Model/EmergencyContact.java
@@ -0,0 +1,73 @@
+package platis.solutions.smartcarmqttcontroller.Model;
+
+public class EmergencyContact {
+
+    private int id;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private int phone_number;
+
+
+    public EmergencyContact(int id, String first_name, String last_name, int phone_number, String email) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.phone_number = phone_number;
+    }
+
+    public EmergencyContact() {
+    }
+
+    @Override
+    public String toString() {
+        return "EmergencyContact{" +
+                "id=" + id +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone_number='" + phone_number + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(int phone_number) {
+        this.phone_number = phone_number;
+    }
+}
