662f7922f8299f9a574a69f2af855ecbc6d2de49
AlbinKarlsson <albin_carlsson@live.se>
2022-05-08 14:42:59 +0200

    add a second method for the joystickCallback

--- android/app/src/main/java/platis/solutions/smartcarmqttcontroller/JoystickView.java
+++ android/app/src/main/java/platis/solutions/smartcarmqttcontroller/JoystickView.java
@@ -103,6 +103,8 @@ public class JoystickView extends SurfaceView implements SurfaceHolder.Callback,
                 //and make the car stop
                 drawJoystick(centerX, centerY);
                 joystickCallback.onJoystickMoved(0, 0, getId());
+                //Added a second one as it seems it responds even better with two joystickCallback
+                joystickCallback.onJoystickMoved(0, 0, getId());
             }
 
         }
